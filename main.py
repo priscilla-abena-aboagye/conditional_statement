87ae59a0bd73faef3464f3b687918910ecea327a
Abena <priscillaabenaaboagye@gmail.com>
2025-07-18 19:58:41 +0000

    printing using for and while loop

--- main.py
+++ main.py
@@ -88,5 +88,18 @@ for i in range(1, rows + 1):
     print("*", end="")
   print()  # Move to a new line after each row of asterisks
 
+a = [1, 2, 3]
+b = [4, 5, 6]
+for i in a:
+	for x in b:
+		print(i, x)
 
+i = 0
+
+while i < len(a):
+    j = 0
+    while j < len(b):
+        print(a[i], b[j])
+        j += 1
+    i += 1
      
